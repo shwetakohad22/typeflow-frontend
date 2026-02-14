8667c0e2dd198380a7c8e8d4bc94d29d3f64bba0
shwetakohad22 <shwetakohad22@gmail.com>
2026-02-14 08:14:36 +0530

    refactor: remove fdir package and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5005",
+        target: "https://typeflow-backend.onrender.com",
         changeOrigin: true,
       },
       "/socket.io": {
-        target: "http://localhost:5005",
+        target: "https://typeflow-backend.onrender.com",
         ws: true,
       },
     },
