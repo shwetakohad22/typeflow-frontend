9a3a9fc9538ec8836704340462ab1e5361dcd729
Shweta Kohad <shwetakohad22@gmail.com>
2026-02-18 12:09:10 +0530

    [ADDED]: base URL

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
+
+if (import.meta.env.PROD) {
+  axios.defaults.baseURL = "https://typeflow-backend.onrender.com";
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
